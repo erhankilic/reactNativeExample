6a61431f540dd618340e085e3565d9d4a92724b9
Erhan Kılıç <erhan@erhankilic.org>
2018-09-27 22:44:14 +0300

    AlbumDetail style and content updated.

--- src/components/albumDetail.js
+++ src/components/albumDetail.js
@@ -1,18 +1,59 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './cart';
 import CardSection from './cardSection';
 
-const Album = (props) => {
-    const { album } = props;
+const Album = ({ album }) => {
+    const { title, artist, image, thumbnail_image } = album;
+    const {
+        albumHeaderStyle,
+        albumHeaderTextStyle,
+        thumbnailStyle,
+        thumbnailContainerStyle,
+        imageStyle
+    } = styles;
 
     return (
         <Card>
             <CardSection>
-                <Text>{album.title}</Text>
+                <View style={thumbnailContainerStyle}>
+                    <Image source={{ uri: thumbnail_image }} style={thumbnailStyle} />
+                </View>
+                <View style={albumHeaderStyle}>
+                    <Text style={albumHeaderTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+            <CardSection>
+                <Image source={{ uri: image }} style={imageStyle} />
             </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    albumHeaderStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    albumHeaderTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        width: 50,
+        height: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
 };
 
 export default Album;
